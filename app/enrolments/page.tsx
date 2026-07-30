1070bbd1c03ecac5d224d5c4d4c6d4f16b5c76b6
sfchong72 <fongmirandah@gmail.com>
2026-07-30 18:55:05 +0800

    Add Student Operations Stage 1A foundation

---
+++ app/enrolments/page.tsx
@@ -0,0 +1,5 @@
+import { StudentOperationsWorkspace } from "@/app/student-operations-workspace";
+
+export default function EnrolmentsPage() {
+  return <StudentOperationsWorkspace mode="enrolments" />;
+}
