12ca3b02c72668dc6f49f4097593a448ab6ec568
sfchong72 <fongmirandah@gmail.com>
2026-07-30 18:54:51 +0800

    Add Student Operations Stage 1A foundation

---
+++ app/programmes/page.tsx
@@ -0,0 +1,5 @@
+import { StudentOperationsWorkspace } from "@/app/student-operations-workspace";
+
+export default function ProgrammesPage() {
+  return <StudentOperationsWorkspace mode="programmes" />;
+}
